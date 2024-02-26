071c6f2ebcd11a5fea128e86f8ba24830daf9cf6
itscharlesm <itscharlesm@gmail.com>
2024-02-26 15:27:48 +0800

    3d1

--- resources/views/admin/appointment/appointment.blade.php
+++ resources/views/admin/appointment/appointment.blade.php
@@ -17,15 +17,70 @@
             <!-- Main Content -->
             <div id="content">
                 @include('layouts.admin.topbar')
-                <!-- Begin Page Content -->
+                <!-- Card -->
+                <div class="card shadow mb-4 mx-4">
+                    <!-- DataTales Example -->
+                    <div class="card shadow mb-4">
+                        <div class="card-header py-3 d-flex align-items-center">
+                            <h2 class="m-0 font-weight-bold text-primary">Clients</h6>
+                                <button class="btn btn-success ml-auto">Register a Customer</button>
+                        </div>
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered responsive" id="dataTable" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th style="text-align: center">ID</th>
+                                            <th style="text-align: center">Last Name</th>
+                                            <th style="text-align: center">First Name</th>
+                                            <th style="text-align: center">Mobile Number</th>
+                                            <th style="text-align: center">Email</th>
+                                            <th style="text-align: center">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>Mendoza</td>
+                                            <td>Kylle Adyzza</td>
+                                            <td>09205433548</td>
+                                            <td>k.mendoza.522854@umindanao.edu.ph</td>
+                                            <td style="text-align: center">
+                                                <!-- View button with primary style -->
+                                                <button class="btn btn-sm btn-primary" data-toggle="tooltip"
+                                                    data-placement="top" title="View">
+                                                    <i class="fas fa-eye"></i>
+                                                </button>
 
-                <!-- End of Main Content -->
+                                                <!-- Edit button with warning style -->
+                                                <button class="btn btn-sm btn-warning" data-toggle="tooltip"
+                                                    data-placement="top" title="Edit">
+                                                    <i class="fas fa-edit"></i>
+                                                </button>
+
+                                                <!-- Delete button with danger style -->
+                                                <button class="btn btn-sm btn-danger" data-toggle="tooltip"
+                                                    data-placement="top" title="Delete">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </td>
+
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End of Main Content -->
+                </div>
+                <!-- End of Content Wrapper -->
             </div>
             <!-- End of Content Wrapper -->
         </div>
         <!-- End of Page Wrapper -->
         @include('layouts.admin.logout')
         @include('components.plugins')
+        @include('components.table')
 </body>
 
 </html>
--- resources/views/admin/client/client.blade.php
+++ resources/views/admin/client/client.blade.php
@@ -23,94 +23,50 @@
                 <div class="card shadow mb-4 mx-4">
                     <!-- DataTales Example -->
                     <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">DataTables Example</h6>
+                        <div class="card-header py-3 d-flex align-items-center">
+                            <h2 class="m-0 font-weight-bold text-primary">Clients</h6>
+                            <button class="btn btn-success ml-auto">Register a Customer</button>
                         </div>
                         <div class="card-body">
                             <div class="table-responsive">
                                 <table class="table table-bordered responsive" id="dataTable" cellspacing="0">
                                     <thead>
                                         <tr>
-                                            <th>Name</th>
-                                            <th>Position</th>
-                                            <th>Office</th>
-                                            <th>Age</th>
-                                            <th>Start date</th>
-                                            <th>Salary</th>
+                                            <th style="text-align: center">ID</th>
+                                            <th style="text-align: center">Last Name</th>
+                                            <th style="text-align: center">First Name</th>
+                                            <th style="text-align: center">Mobile Number</th>
+                                            <th style="text-align: center">Email</th>
+                                            <th style="text-align: center">Action</th>
                                         </tr>
                                     </thead>
                                     <tbody>
                                         <tr>
-                                            <td>Tiger Nixon</td>
-                                            <td>System Architect</td>
-                                            <td>Edinburgh</td>
-                                            <td>61</td>
-                                            <td>2011/04/25</td>
-                                            <td>$320,800</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Garrett Winters</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>63</td>
-                                            <td>2011/07/25</td>
-                                            <td>$170,750</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Ashton Cox</td>
-                                            <td>Junior Technical Author</td>
-                                            <td>San Francisco</td>
-                                            <td>66</td>
-                                            <td>2009/01/12</td>
-                                            <td>$86,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Cedric Kelly</td>
-                                            <td>Senior Javascript Developer</td>
-                                            <td>Edinburgh</td>
-                                            <td>22</td>
-                                            <td>2012/03/29</td>
-                                            <td>$433,060</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Airi Satou</td>
-                                            <td>Accountant</td>
-                                            <td>Tokyo</td>
-                                            <td>33</td>
-                                            <td>2008/11/28</td>
-                                            <td>$162,700</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Brielle Williamson</td>
-                                            <td>Integration Specialist</td>
-                                            <td>New York</td>
-                                            <td>61</td>
-                                            <td>2012/12/02</td>
-                                            <td>$372,000</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Herrod Chandler</td>
-                                            <td>Sales Assistant</td>
-                                            <td>San Francisco</td>
-                                            <td>59</td>
-                                            <td>2012/08/06</td>
-                                            <td>$137,500</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Rhona Davidson</td>
-                                            <td>Integration Specialist</td>
-                                            <td>Tokyo</td>
-                                            <td>55</td>
-                                            <td>2010/10/14</td>
-                                            <td>$327,900</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Colleen Hurst</td>
-                                            <td>Javascript Developer</td>
-                                            <td>San Francisco</td>
-                                            <td>39</td>
-                                            <td>2009/09/15</td>
-                                            <td>$205,500</td>
+                                            <td>1</td>
+                                            <td>Mendoza</td>
+                                            <td>Kylle Adyzza</td>
+                                            <td>09205433548</td>
+                                            <td>k.mendoza.522854@umindanao.edu.ph</td>
+                                            <td style="text-align: center">
+                                                <!-- View button with primary style -->
+                                                <button class="btn btn-sm btn-primary" data-toggle="tooltip"
+                                                    data-placement="top" title="View">
+                                                    <i class="fas fa-eye"></i>
+                                                </button>
+
+                                                <!-- Edit button with warning style -->
+                                                <button class="btn btn-sm btn-warning" data-toggle="tooltip"
+                                                    data-placement="top" title="Edit">
+                                                    <i class="fas fa-edit"></i>
+                                                </button>
+
+                                                <!-- Delete button with danger style -->
+                                                <button class="btn btn-sm btn-danger" data-toggle="tooltip"
+                                                    data-placement="top" title="Delete">
+                                                    <i class="fas fa-trash"></i>
+                                                </button>
+                                            </td>
+
                                         </tr>
                                     </tbody>
                                 </table>
